29842d910e227de2e8bf7801389b7b8fbd2b4aab
Navya <navyabynaboina02@gmail.com>
2024-11-06 23:55:54 +0530

    Added new file in Arrays

---
+++ src/Arrays/KdanesAlgorithm.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class KdanesAlgorithm {
+    public static void main(String[] args){
+        int[] nums={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+    public static int maxSubArray(int[] nums) {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            sum+=nums[i];
+            max=Math.max(sum,max);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return max;
+
+    }
+}
